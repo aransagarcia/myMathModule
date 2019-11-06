d16290e8b5777cdd0539e63d18e2b2bd6e77fb58
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2019-11-05 20:51:59 -0500

    added div function to the math object

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-/* 
+	/* 
   myMathModule
-
   This is a collaboratively made Math module with support for basic math operations.
   Feel free to use it for you basic math needs.
 */
+
+let div = require('./div');
 const Math = {
-    PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+    PI: 3.141592653589793, // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+    div: div
   }
   
   module.exports = Math
