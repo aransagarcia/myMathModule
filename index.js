d95f8ac9f999dc2adc057bc0f4ce97889245cbab
your name here <your_email_here>
2019-11-05 21:02:22 -0500

    added multiplication

--- index.js
+++ index.js
@@ -4,9 +4,13 @@
   This is a collaboratively made Math module with support for basic math operations.
   Feel free to use it for you basic math needs.
 */
+
+const sum = require('/multiply.js')
 const Math = {
-    PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+    PI: 3.141592653589793, // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+    multiply: multiply
   }
   
   module.exports = Math
+  
   
