95684661c1705a45a33326c738b09ceb9c8d9251
aransagarcia <aransagarcia@pursuit.org>
2019-11-05 20:53:18 -0500

    added sum function

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
   This is a collaboratively made Math module with support for basic math operations.
   Feel free to use it for you basic math needs.
 */
+const sum = require('./sum.js')
 const Math = {
     PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
-  }
+  sum:sum
+}
   
   module.exports = Math
-  
