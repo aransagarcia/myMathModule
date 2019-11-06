d06b185e21fef12a62203317a667b05c0f23e291
Hupaul <hupaulcamacho@pursuit.org>
2019-11-05 20:52:32 -0500

    add module to object

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
   This is a collaboratively made Math module with support for basic math operations.
   Feel free to use it for you basic math needs.
 */
+const sub = require('./sub.js')
 const Math = {
-    PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+    PI: 3.141592653589793, // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+    sub: sub
   }
   
-  module.exports = Math
+module.exports = Math
   
